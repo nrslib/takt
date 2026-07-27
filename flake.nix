07df41b2653eb98b5dfbd8846d102acbc2a76212
nrslib <38722970+nrslib@users.noreply.github.com>
2026-07-27 09:34:12 +0900

    fix: Nix依存ハッシュを更新する

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
             version = packageJson.version;
             src = ./.;
 
-            npmDepsHash = "sha256-a2N2GmfNhhq55qd9P3sYHDCVJuFC3y8LHVDIGbMVrEM=";
+            npmDepsHash = "sha256-dlbV3QO5oXcWWQLoE0AHQA0IVwWxINdeWtZzEVLUHXA=";
             nodejs = nodejs;
             ONNXRUNTIME_NODE_INSTALL = "skip";
             PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
