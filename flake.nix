babcd325643d2e1c346da2d595ebdddf5fa49eb1
nrslib <38722970+nrslib@users.noreply.github.com>
2026-08-21 19:10:01 +0900

    chore(nix): package-lock更新に合わせてnpmDepsHashを更新する

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
             version = packageJson.version;
             src = ./.;
 
-            npmDepsHash = "sha256-L2dC09OZWYCIK9X8XOXX0x6hLfKOZvZoDcj1cTgq6r4=";
+            npmDepsHash = "sha256-5SkM5JtSBdNuS678PmWSCqL55qnvqa2RLWm9LsyaED8=";
             npmDepsFetcherVersion = 2;
             nodejs = nodejs;
             ONNXRUNTIME_NODE_INSTALL = "skip";
